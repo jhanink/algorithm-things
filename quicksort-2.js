518fc5a3e9da119348aa801b8e5a81aa85c6bc9e
Joe Hanink <jhanink@gmail.com>
2016-02-04 15:09:50 -0800

    add compact version of quicksort

---
+++ quicksort-2.js
@@ -0,0 +1,22 @@
+var quicksort = function(a) {
+  var pivot = a[Math.floor(a.length/2)];
+  var lt=[], gt=[], eq=[];
+  for (var i=0;i<a.length;i++) {
+    if (a[i]<pivot) lt.push(a[i]);
+    if (a[i]==pivot) eq.push(pivot);
+    if (a[i]>pivot) gt.push(a[i]);
+  }
+  return lt.sort().concat(eq, gt.sort());
+}
+
+
+
+var randarray = function(arr, n) {
+  if (n===0) return arr;
+  arr.push(Math.floor(Math.random()*100000));
+  return randarray(arr, n-1);
+}
+
+var a = new Date().getTime();
+console.log(quicksort(randarray([], 10000)))
+console.log("time:", new Date().getTime()-a)
