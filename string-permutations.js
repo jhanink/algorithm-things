64592afcbd2f517c62b903869637f30e8c1b1a04
Joe Hanink <jhanink@gmail.com>
2016-02-08 21:53:32 -0800

    rename to string-permutations.js

--- string-permutations.js
+++ string-permutations.js
@@ -1,10 +1,10 @@
 var genPerms = function(a) {
   if (a.length === 1) return [a];
   var last = a.slice(-1);
-  var remainingPerms = genPerms(a.slice(0,-1));
+  var nextPerms = genPerms(a.slice(0,-1));
   var perms = [];
-  for (var i=0;i<remainingPerms.length;i++) {
-    var next = remainingPerms[i];
+  for (var i=0;i<nextPerms.length;i++) {
+    var next = nextPerms[i];
     for (var j=0;j<=next.length;j++) {
       perms.push(next.slice(0,j) + last + next.slice(j,next.length));
     }
