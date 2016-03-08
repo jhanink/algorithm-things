1551a3472fa31851dc347c2786b759ab6575426c
Joe Hanink <jhanink@gmail.com>
2016-03-07 22:20:58 -0800

    add iterative version of is-palindrome

--- extras/is-palindrome.js
+++ extras/is-palindrome.js
@@ -6,7 +6,21 @@ var isPalindrome = function(str) {
 };
 
 // ---------- test
+console.log("isPalindrome recursive");
 console.log("abc",isPalindrome("abc"));
 console.log("abccba",isPalindrome("abccba"));
 console.log("abcdcba",isPalindrome("abcdcba"));
+
+var isPalindrome2 = function(str) {
+  var half = Math.floor(str.length/2);
+  for (var i=0;i<half;i++) {
+    if (str.charAt(i) !== str.charAt(str.length-1-i)) return false;
+  }
+  return true;
+};
+
+console.log("\nisPalindrome iterative");
+console.log("abc",isPalindrome2("abc"));
+console.log("abccba",isPalindrome2("abccba"));
+console.log("abcdcba",isPalindrome2("abcdcba"));
 
